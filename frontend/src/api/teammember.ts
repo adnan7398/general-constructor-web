a7d505ab0927a1e355a891d8f3c9abd74808f58b
adnan7398 <123adnansiddiqui@gmail.com>
2025-07-11 02:25:12 +0530

    api base url

--- frontend/src/api/teammember.ts
+++ frontend/src/api/teammember.ts
@@ -18,8 +18,8 @@ const headers = {
   Authorization: `Bearer ${token}`,
   'Content-Type': 'application/json',
 }
-const API_BASE_URL = 'http://localhost:3000/team';
-const API_BASE_URL_USER = 'http://localhost:3000/user';
+const API_BASE_URL = 'https://general-constructor-web-2.onrender.com';
+const API_BASE_URL_USER = 'https://general-constructor-web-2.onrender.com/user';
 export const getTeamMembers = async (): Promise<TeamMember[]> => {
   const response = await axios.get(API_BASE_URL,{
     headers:headers
