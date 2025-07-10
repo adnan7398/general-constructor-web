cb1920a2e4eceddcf9cb53f00befe1e876f0d5e0
adnan7398 <123adnansiddiqui@gmail.com>
2025-07-11 03:47:58 +0530

    team added

--- frontend/src/api/teammember.ts
+++ frontend/src/api/teammember.ts
@@ -27,8 +27,8 @@ export const getTeamMembers = async (): Promise<TeamMember[]> => {
   return response.data;
 };
 
-export const addTeamMember = async (member: Omit<TeamMember, '_id'>): Promise<TeamMember> => {
-  const response = await axios.post(API_BASE_URL, member,{
+export const addTeamMember = async (): Promise<TeamMember> => {
+  const response = await axios.post(API_BASE_URL,{
     headers: headers
   });
   return response.data;
