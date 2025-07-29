8e4b737d2e3849ec2fbcf6bbb27e7c48e250fbd1
adnan7398 <123adnansiddiqui@gmail.com>
2025-07-30 02:44:55 +0530

    signin fix

--- frontend/src/auth/Signin.tsx
+++ frontend/src/auth/Signin.tsx
@@ -18,7 +18,7 @@ const SignIn: React.FC = () => {
     }
   
     try {
-      const response = await fetch(`${API_BASE_URL}/login`, {
+      const response = await fetch(`${API_BASE_URL}/admin/login`, {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json'
